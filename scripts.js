fc1b49b0928b66c5ba399e0e1eac00d24c538ae6
Anzelah <jepkoechanzellah@gmail.com>
2024-05-18 22:05:22 +0300

    solved the button funtionality. Now working perfecttly

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
 const myButton = document.querySelector('button');
-const myInput = document.getElementById('myInput');
+const myInput = document.querySelector('input');
 const myUl = document.querySelector('ul');
 
 myButton.addEventListener('click', () => {
 	let toDos = myInput.value;
 	localStorage.setItem('list', toDos);
+	myInput.value = ''
 
-	const frag = document.createDocumentFragment();
 	const li = document.createElement('li')
-	frag.appendChild(li)
-
 	const storedList = localStorage.getItem('list');
 	if (storedList) {
 		li.textContent = storedList
 	} else {
 		li.textContent = ''
 	}
-	document.myUl.appendChild(frag)
+	myUl.appendChild(li)
 });

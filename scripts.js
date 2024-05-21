e6d864949a3d4001d98839c690a96d3756798c8a
Anzelah <jepkoechanzellah@gmail.com>
2024-05-21 15:45:44 +0300

    was overwriting existing list everytime i clicked the button. So had to retrieve list first before the click event

--- scripts.js
+++ scripts.js
@@ -2,17 +2,30 @@ const myButton = document.querySelector('button');
 const myInput = document.querySelector('input');
 const myUl = document.querySelector('ul');
 
-myButton.addEventListener('click', () => {
-	let toDos = myInput.value;
-	localStorage.setItem('list', toDos);
-	myInput.value = ''
+/* Load existing list or create one if none */
+const existingList = JSON.parse(localStorage.getItem('list')) || [];
 
+/* display existing to-dos */
+existingList.forEach((item) => {
 	const li = document.createElement('li')
-	const storedList = localStorage.getItem('list');
-	if (storedList) {
-		li.textContent = storedList
-	} else {
-		li.textContent = ''
-	}
+	li.textContent = item
 	myUl.appendChild(li)
+});
+
+myButton.addEventListener('click', () => {
+	/* extract the new todo */
+	const newTodo = myInput.value;
+
+	/* if its a valid entry */
+	if (newTodo !== '') {
+		existingList.push(newTodo)
+		/* save updated list to local storage */
+		localStorage.setItem('list', JSON.stringify(existingList));
+
+		const li = document.createElement('li')
+		li.textContent = newTodo
+		
+		myUl.appendChild(li)
+		myInput.value = ''
+	}
 });

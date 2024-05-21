5022cb6e454f7f4d88d7af30850383e4c27891af
Anzelah <jepkoechanzellah@gmail.com>
2024-05-21 18:39:31 +0300

    when li item is clicked, it marks it as done

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const myButton = document.querySelector('button');
 const myInput = document.querySelector('input');
 const myUl = document.querySelector('ul');
 
+
+// GET EXISTING TODOS & CREATE LIST ITEM FOR EACH
 /* Load existing list or create one if none */
 const existingList = JSON.parse(localStorage.getItem('list')) || [];
 
@@ -12,7 +14,8 @@ existingList.forEach((item) => {
 	myUl.appendChild(li)
 });
 
-myButton.addEventListener('click', () => {
+// WHEN YOU CLICK A BUTTON
+function addTodos() {
 	/* extract the new todo */
 	const newTodo = myInput.value;
 
@@ -23,9 +26,24 @@ myButton.addEventListener('click', () => {
 		localStorage.setItem('list', JSON.stringify(existingList));
 
 		const li = document.createElement('li')
-		li.textContent = newTodo
-		
+		li.textContent = newTodo		
 		myUl.appendChild(li)
+
 		myInput.value = ''
 	}
+};
+myButton.addEventListener('click', addTodos);
+
+
+// WHEN YOU CLICK A LIST ITEM
+
+const lst = document.querySelectorAll('li')
+
+lst.forEach(item => {
+	item.addEventListener('click', () => {
+		item.style.textDecoration = 'line-through'
+		//item.style.backgroundImage = 'url(images/checked.png)'
+	})
 });
+
+
